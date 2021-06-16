949c264dc4283370e988b705eb3b05870870095e
Canh Do Minh <minhcanh99@gmail.com>
2021-06-16 21:21:09 +0900

    Add examples for experiments

--- sequential/test-command.sh
+++ sequential/test-command.sh
@@ -1,4 +1,7 @@
 #!/bin/zsh
 time maude << EOF
-in Needham_Schroeder.maude
+in maude-npa.maude
+in ../examples/Needham_Schroeder_Lowe_ECB.maude
+select MAUDE-NPA .
+red run(0, unbounded) .
 EOF
--- test-command.sh
+++ test-command.sh
@@ -1,4 +1,9 @@
 #!/bin/zsh
 time maude << EOF
-in Needham_Schroeder.maude
+in parallel-maude-npa.maude
+in examples/Needham_Schroeder_Lowe_ECB.maude
+select PARALLEL-MAUDE-NPA .
+set print attribute on .
+erew <> < aManager : Manager | nWorkers : 4, status : idle >
+        p-run(0, unbounded) .
 EOF
