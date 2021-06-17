3d0db8db994b0ae24ba51eb4bc82ac6d4aa91b01
Canh Do Minh <minhcanh99@gmail.com>
2021-06-17 21:57:37 +0900

    Should states be filtered at each worker

--- test-command.sh
+++ test-command.sh
@@ -3,7 +3,6 @@ time maude << EOF
 in parallel-maude-npa.maude
 in examples/Needham_Schroeder_Lowe_ECB.maude
 select PARALLEL-MAUDE-NPA .
-set print attribute on .
-erew <> < aManager : Manager | nWorkers : 4, status : idle >
-        p-run(0, unbounded) .
+set print attribute off .
+erew <> p-run(0, unbounded, 10) .
 EOF
