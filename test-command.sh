f617b8e6b66b4e206535156f32444a8e89d26d30
Canh Do Minh <minhcanh99@gmail.com>
2021-06-21 23:02:18 +0900

    Parallel with depth

--- test-command.sh
+++ test-command.sh
@@ -3,6 +3,6 @@ time maude << EOF
 in parallel-maude-npa.maude
 in examples/Needham_Schroeder.maude
 select PARALLEL-MAUDE-NPA .
-set print attribute on .
-erew <> p-run(0, unbounded, 4, 3) .
+set print attribute off .
+erew <> p-run(0, unbounded, 10, 6) .
 EOF
