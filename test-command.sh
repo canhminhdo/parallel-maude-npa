b58df0fd45fd06d17c1c12e68d2d3b38ecd01622
Canh Do Minh <minhcanh99@gmail.com>
2021-07-13 10:25:36 +0900

    Debugging program

--- test-command.sh
+++ test-command.sh
@@ -4,5 +4,5 @@ in parallel-maude-npa.maude
 in examples/Needham_Schroeder.maude
 select PARALLEL-MAUDE-NPA .
 set print attribute on .
-erew <> p-run(0, unbounded, 8) .
+erew <> p-run(0, unbounded, 2) .
 EOF
