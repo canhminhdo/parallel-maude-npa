b49f4c2918b2c08bcb449fab2fa9acf2568cec37
Canh Do Minh <minhcanh99@gmail.com>
2021-08-23 17:13:26 +0900

    Parallel Maude-NPA with meta-interperters

--- sequential/test-command.sh
+++ sequential/test-command.sh
@@ -1,7 +1,29 @@
 #!/bin/zsh
 time maude << EOF
 in maude-npa.maude
-in ../examples/Needham_Schroeder_Lowe_ECB.maude
+load examples/API-Protocols/YubiKey/YubiKey.maude
 select MAUDE-NPA .
-red run(0, unbounded) .
+red run(21, 8) .
 EOF
+
+# red run(0, 7) .
+# red summary(1) .
+# red summary(2) .
+# red summary(3) .
+# red summary(4) .
+# red summary(5) .
+# red summary(6) .
+# red summary(7) .
+# red summary(8) .
+# red summary(9) .
+# red summary(10) .
+# red summary(11) .
+# red summary(12) .
+# red summary(13) .
+# red summary(14) .
+# red summary(15) .
+# red summary(16) .
+# red summary(17) .
+# red summary(18) .
+# red summary(19) .
+# red summary(20) .
--- test-command.sh
+++ test-command.sh
@@ -1,8 +1,9 @@
 #!/bin/zsh
 time maude << EOF
 load debug.maude
+load maude-npa.maude
 load parallel-maude-npa.maude
 load examples/Needham_Schroeder.maude
 select PARALLEL-MAUDE-NPA .
-erew <> p-run(0, unbounded, 4) .
+erew <> p-run(0, unbounded, 8) .
 EOF
