88638d4a87658ac389beeab8ed75a7453920b702
Canh Do Minh <minhcanh99@gmail.com>
2021-08-27 16:44:51 +0900

    Update for experiments

--- server-command.sh
+++ server-command.sh
@@ -4,6 +4,6 @@ in server.maude
 load examples/Exclusive-OR-Protocols/tmn/ltv-F-tmn-asy.maude
 select PARALLEL-MAUDE-NPA .
 load debug.maude
-erew <> < aServer : Server | status : idle, attack : 0, bStep : unbounded, step : 1, #workers : 4 >
+erew <> < aServer : Server | status : idle, attack : 0, bStep : unbounded, step : 1, #workers : 8 >
         CreateServerTcpSocket(socketManager, aServer, 9000, 10) .
 EOF
