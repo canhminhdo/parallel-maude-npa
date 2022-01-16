565e6740589e46a074c1e32566adfacaf212e625
Canh Do Minh <minhcanh99@gmail.com>
2022-01-17 00:08:44 +0900

    Fix maude-npa for meta-interpreter

--- test-command.sh
+++ test-command.sh
@@ -3,7 +3,7 @@ time maude << EOF
 load debug.maude
 load maude-npa.maude
 load parallel-maude-npa.maude
-load examples/Needham_Schroeder.maude
+load examples/Symmetric-Key-Protocols/Diffie-Hellman/Diffie-Hellman.maude
 select PARALLEL-MAUDE-NPA .
 erew <> p-run(0, unbounded, 8) .
 EOF
