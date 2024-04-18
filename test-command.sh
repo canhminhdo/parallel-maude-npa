692b9c5edfc8d1501e412518366940417e3c89e0
Canh Minh Do <minhcanh99@gmail.com>
2024-04-18 16:39:47 +0900

    Turn on memory optimization by a global option

--- test-command.sh
+++ test-command.sh
@@ -3,7 +3,9 @@ time maude << EOF
 load debug.maude
 load maude-npa.maude
 load parallel-maude-npa.maude
-load examples/Homomorphism-Protocols/Needham_Schroeder_Lowe_ECB/Needham_Schroeder_Lowe_ECB.maude
+load examples/Symmetric-Key-Protocols/Amended_Needham-Schroeder/Amended_Needham-Schroeder.maude
 select PARALLEL-MAUDE-NPA .
-erew <> p-run(0, unbounded, 8) .
+erew <> p-run(0, unbounded, 24) .
 EOF
+
+# examples/API-Protocols/YubiKey/YubiKey.maude
