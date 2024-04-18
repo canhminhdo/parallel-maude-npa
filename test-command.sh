81528d9fef472e90c15d63b4f8607c46a552e217
Canh Minh Do <minhcanh99@gmail.com>
2024-04-18 17:32:11 +0900

    Ready for testing

--- test-command.sh
+++ test-command.sh
@@ -3,7 +3,11 @@ time maude << EOF
 load debug.maude
 load maude-npa.maude
 load parallel-maude-npa.maude
-load examples/Homomorphism-Protocols/Needham_Schroeder_Lowe_ECB/Needham_Schroeder_Lowe_ECB.maude
+load examples/Symmetric-Key-Protocols/Amended_Needham-Schroeder/Amended_Needham-Schroeder.maude
 select PARALLEL-MAUDE-NPA .
-erew <> p-run(0, unbounded, 8) .
+erew <> p-run(0, unbounded, 24) .
 EOF
+
+# load examples/Symmetric-Key-Protocols/Amended_Needham-Schroeder/Amended_Needham-Schroeder.maude
+# load examples/API-Protocols/YubiKey/YubiKey.maude
+# load examples/Choice-Protocols/TLS-regular.maude
